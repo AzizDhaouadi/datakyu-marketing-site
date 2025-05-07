466257c4bb961ba6aa64cd024322abb31ad3f660
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-05-06 20:34:12 -0400

    Created an alternate timeline component

---
+++ components/custom/AlternatingTimeline.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Timeline, Flex, Text, Box } from "@chakra-ui/react";
+
+type TimelineStepProps = {
+  name: string;
+};
+
+export default function AlternatingTimeline({
+  steps,
+}: {
+  steps: TimelineStepProps[];
+}) {
+  return (
+    <Timeline.Root size="xl" variant="outline" gap={10}>
+      {steps.map((step, index) => (
+        <Timeline.Item key={index}>
+          {/* Left-side content (for odd indices) */}
+          <Timeline.Content flex="1" alignItems="flex-end">
+            {index % 2 !== 0 && (
+              <Flex alignItems="center" justifyContent="flex-end" gap={3}>
+                <Text fontWeight="semibold">{step.name}</Text>
+              </Flex>
+            )}
+          </Timeline.Content>
+
+          {/* Middle indicator and connector */}
+          <Timeline.Connector>
+            <Timeline.Separator />
+            <Timeline.Indicator />
+          </Timeline.Connector>
+
+          {/* Right-side content (for even indices) */}
+          <Timeline.Content flex="1">
+            {index % 2 === 0 && (
+              <Flex alignItems="center" gap={3}>
+                <Text fontWeight="semibold">{step.name}</Text>
+              </Flex>
+            )}
+          </Timeline.Content>
+        </Timeline.Item>
+      ))}
+    </Timeline.Root>
+  );
+}
