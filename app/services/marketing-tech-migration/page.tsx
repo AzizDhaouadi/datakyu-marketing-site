196d69310c7586d95a33fa3dcbebbe1607b67003
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-05-08 21:32:00 -0400

    Refactor MarketingTechMigration page to add TypedCodeSection and ListsWithIcons components

--- app/services/marketing-tech-migration/page.tsx
+++ app/services/marketing-tech-migration/page.tsx
@@ -12,17 +12,62 @@ import { Text, Heading, Flex, Box } from "@chakra-ui/react";
 
 // 🧩 Custom Components – Core
 import Nav from "@/components/custom/Nav";
-import HeroHeading from "@/components/custom/HeroHeading";
 import CallToActionButton from "@/components/custom/CallToActionButton";
+import ListsWithIcons from "@/components/custom/ListsWithIcons";
 import Footer from "@/components/custom/Footer";
 
+// 🎞️ Custom Components – Animations
+import TypedCodeSection from "@/components/animation/TypedCode";
+import { desc } from "framer-motion/client";
+
 export default function MarketingTechMigration() {
+  const codeToType = [
+    "Helping every business.",
+    "Helping every team.",
+    "Helping every startup.",
+    "Helping every decision maker.",
+  ];
+
+  const listsContent = [
+    {
+      title: "Google Analytics 4",
+      description:
+        "Google Analytics 4 is the latest version of Google Analytics, which uses event-based data collection and machine learning to provide insights into user behavior across websites and apps.",
+      icon: null,
+    },
+    {
+      title: "Segment",
+      description:
+        "Segment is a customer data platform (CDP) that helps businesses collect, clean, and control their customer data. It allows you to send data to various marketing and analytics tools.",
+      icon: null,
+    },
+    {
+      title: "Mixpanel",
+      description:
+        "Mixpanel is an advanced analytics platform that helps businesses track user interactions with web and mobile applications. It provides insights into user behavior, engagement, and retention.",
+      icon: null,
+    },
+  ];
+
   return (
     <div className={styles.page}>
       <header style={{ width: "100%" }} className={styles.header}>
         <Nav />
       </header>
-      <main className={styles.main}>{/* <h1>Hello world</h1> */}</main>
+      <main className={styles.main}>
+        <Flex direction={"row"} gap={6}>
+          <Flex direction={"column"} gap={4}>
+            <TypedCodeSection codeToType={codeToType} smartBackspace={true} />
+            <Text p={4}>
+              Build a tracking infrastructure that scales with your business.
+              Future proof your tracking and work with tooks that are shaping to
+              future of web analytics.
+            </Text>
+            <ListsWithIcons listDescriptions={listsContent} />
+          </Flex>
+          <Flex></Flex>
+        </Flex>
+      </main>
     </div>
   );
 }
