49cb2584f9465c8f8a043d8793e4bf31f7ee9781
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-05-10 18:31:10 -0400

    Refactor CustomMarketingAnalytics page to include ListsWithIcons component and code highlighting

--- app/services/custom-marketing-analytics/page.tsx
+++ app/services/custom-marketing-analytics/page.tsx
@@ -7,10 +7,16 @@ import HeroHeading from "@/components/custom/HeroHeading";
 import CardsWithImages from "@/components/custom/CardsWithImages";
 import CallToActionButton from "@/components/custom/CallToActionButton";
 import CustomDialog from "@/components/custom/Dialog";
+import ListsWithIcons from "@/components/custom/ListsWithIcons";
 import Footer from "@/components/custom/Footer";
 
 // 💅 Chakra UI
-import { Text, Heading, Image, Flex } from "@chakra-ui/react";
+import { Text, Heading, Image, Flex, Box } from "@chakra-ui/react";
+
+// 💻 Code Hightlighter
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-tomorrow.css";
 
 export default function CustomMarketingAnalytics() {
   const dialogsContent = [
@@ -93,6 +99,53 @@ export default function CustomMarketingAnalytics() {
     },
   ];
 
+  const listsContent = [
+    {
+      title: "Google Analytics 4",
+      description:
+        "Google Analytics 4 is the latest version of Google Analytics, which uses event-based data collection and machine learning to provide insights into user behavior across websites and apps.",
+      icon: null,
+    },
+    {
+      title: "Segment",
+      description:
+        "Segment is a customer data platform (CDP) that helps businesses collect, clean, and control their customer data. It allows you to send data to various marketing and analytics tools.",
+      icon: null,
+    },
+    {
+      title: "Mixpanel",
+      description:
+        "Mixpanel is an advanced analytics platform that helps businesses track user interactions with web and mobile applications. It provides insights into user behavior, engagement, and retention.",
+      icon: null,
+    },
+  ];
+
+  const codeContent = [
+    `gtag("event",
+    "submitted_form",{
+    "form_id":"contact_form",
+    "session_number":4
+});`,
+
+    `
+window.dataLayer.push({
+    "event":"submitted_form",
+    "form_id":"contact_form",
+    "session_number":4
+});`,
+    `
+mixpanel.track(
+    "submitted_form",{
+    "form_id":"contact_form",
+    "session_number":4
+});
+    `,
+  ];
+
+  const highlightedCodeContent = codeContent.map((code) => {
+    return Prism.highlight(code, Prism.languages.javascript, "javascript");
+  });
+
   return (
     <div className={styles.page}>
       <header style={{ width: "100%" }} className={styles.header}>
@@ -153,6 +206,61 @@ export default function CustomMarketingAnalytics() {
           <Text textAlign={"center"}>All the different ways we can help.</Text>
           <CardsWithImages cardsContent={featureCardsContent} />
           <CallToActionButton text={"Contact us"} toCenter={true} />
+        </section>
+        <section style={{ marginTop: "2rem" }} id={"analytics-tech-stack"}>
+          <Flex direction={"row"} gap={6}>
+            <Flex direction={"column"} gap={6}>
+              <Heading
+                my={6}
+                color={"rgb(79, 59, 62)"}
+                size={"4xl"}
+                textAlign={"left"}
+                letterSpacing={"tight"}
+              >
+                Our Technology Stack
+              </Heading>
+              <Text>
+                Build a tracking infrastructure that scales with your business.
+                Future proof your tracking and work with tooks that are shaping
+                to future of web analytics.
+              </Text>
+              <ListsWithIcons listDescriptions={listsContent} />
+            </Flex>
+            <Flex
+              display={{ base: "none", md: "none", lg: "flex" }}
+              width={{ lg: "35rem" }}
+            >
+              <Box
+                bg="gray.900"
+                w={"55rem"}
+                mx={"auto"}
+                py={4}
+                px={2}
+                borderRadius="xl"
+              >
+                <Box
+                  bg="gray.800"
+                  p={6}
+                  borderRadius="md"
+                  color="white"
+                  overflowX="hidden"
+                  height={"100%"}
+                  style={{
+                    fontFamily: "var(--font-mono)",
+                    fontSize: "0.9rem",
+                    whiteSpace: "pre",
+                  }}
+                >
+                  {highlightedCodeContent.map((code: any, index) => (
+                    <Box
+                      key={index}
+                      dangerouslySetInnerHTML={{ __html: code }}
+                    ></Box>
+                  ))}
+                </Box>
+              </Box>
+            </Flex>
+          </Flex>
         </section>
       </main>
     </div>
