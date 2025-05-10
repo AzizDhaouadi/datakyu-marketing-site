18d466b5aa338221ea368a1a5dd48e65b626e41d
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-05-10 01:37:00 -0400

    Refactor ListsWithIcons component to conditionally render item icons and add reverse option

--- components/custom/ListsWithIcons.tsx
+++ components/custom/ListsWithIcons.tsx
@@ -5,17 +5,33 @@ type ListsWithIconsProps = {
   title: string;
   description: string;
   icon: any;
+  reverse?: boolean;
 };
 
 export default function ListsWithIcons({
   listDescriptions,
+  toReverse,
 }: {
   listDescriptions: ListsWithIconsProps[];
+  toReverse?: boolean;
 }) {
   return (
-    <Stack direction={"column"} gap={5}>
+    <Stack
+      direction={toReverse ? "row" : "column"}
+      wrap={toReverse ? "wrap" : "initial"}
+      gap={5}
+      justifyContent={{ lg: "center", xl: "center" }}
+    >
       {listDescriptions.map((item, index) => (
-        <Flex key={index} direction={"row"} gap={5}>
+        <Flex
+          w={{
+            md: toReverse ? "30rem" : "inherit",
+            lg: toReverse ? "40rem" : "inherit",
+          }}
+          key={index}
+          direction={item.reverse ? "column" : "row"}
+          gap={2}
+        >
           {item.icon && (
             <Box alignItems={"center"} display={"flex"} boxSize={10}>
               {item.icon}
