e2f573490b4689abcda96b584d6f4dbfd6f77be6
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-05-17 22:38:07 -0400

    Refactor TypedCodeSection component to improve code highlighting and typing animation in TypedCode.tsx

--- components/animation/TypedCode.tsx
+++ components/animation/TypedCode.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import React from "react";
 import { Box, SkeletonText } from "@chakra-ui/react";
 import Typed from "typed.js";
@@ -12,32 +12,70 @@ type TypedCodeSectionProps = {
   smartBackspace?: boolean;
   className?: string;
   isHeader?: boolean;
+  isGen?: boolean;
 };
 
 export default function TypedCodeSection({
   codeToType,
   smartBackspace,
   isHeader,
+  isGen,
 }: TypedCodeSectionProps) {
   const el = React.useRef(null);
 
-  let highlightedCodeToType = "";
+  // let highlightedCodeToType = "";
 
-  if (typeof codeToType == "string") {
-    highlightedCodeToType = Prism.highlight(
-      codeToType,
-      Prism.languages.javascript,
-      "javascript"
-    );
-  }
+  // if (typeof codeToType == "string") {
+  //   highlightedCodeToType = Prism.highlight(
+  //     codeToType,
+  //     Prism.languages.javascript,
+  //     "javascript"
+  //   );
+  // }
 
-  let highlightedCodes: string[] = [];
+  const highlightedCodeToType = useMemo(() => {
+    if (typeof codeToType === "string") {
+      return Prism.highlight(
+        codeToType,
+        Prism.languages.javascript,
+        "javascript"
+      );
+    }
+    return "";
+  }, [codeToType]);
 
-  if (Array.isArray(codeToType)) {
-    highlightedCodes = codeToType.map((code: string) =>
-      Prism.highlight(code, Prism.languages.javascript, "javascript")
-    );
-  }
+  // let highlightedCodes: string[] = [];
+
+  // if (Array.isArray(codeToType)) {
+  //   highlightedCodes = codeToType.map((code: string) =>
+  //     Prism.highlight(code, Prism.languages.javascript, "javascript")
+  //   );
+  // }
+
+  const highlightedCodes = useMemo(() => {
+    if (Array.isArray(codeToType)) {
+      return codeToType.map((code) =>
+        Prism.highlight(code, Prism.languages.javascript, "javascript")
+      );
+    }
+    return [];
+  }, [codeToType]);
+
+  // useEffect(() => {
+  //   const typed = new Typed(el.current, {
+  //     strings: !smartBackspace ? [highlightedCodeToType] : highlightedCodes,
+  //     typeSpeed: 50,
+  //     showCursor: false,
+  //     contentType: "html",
+  //     smartBackspace: smartBackspace,
+  //     loop: smartBackspace ? true : false,
+  //   });
+
+  //   return () => {
+  //     // Destroy Typed instance during cleanup to stop animation
+  //     typed.destroy();
+  //   };
+  // }, [highlightedCodeToType, highlightedCodes]);
 
   useEffect(() => {
     const typed = new Typed(el.current, {
@@ -49,14 +87,12 @@ export default function TypedCodeSection({
       loop: smartBackspace ? true : false,
     });
 
-    return () => {
-      // Destroy Typed instance during cleanup to stop animation
-      typed.destroy();
-    };
-  }, []);
+    return () => typed.destroy();
+  }, [highlightedCodeToType, highlightedCodes, smartBackspace]);
+
   return (
     <Box ref={el} p={isHeader ? 0 : 4} pl={isHeader ? 2 : 0} borderRadius="md">
-      <SkeletonText noOfLines={1} gap="4" />
+      {isGen ? <></> : <SkeletonText noOfLines={1} gap="4" />}
     </Box>
   );
 }
